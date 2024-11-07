d910cb607fe9abcbac38dddd7e7508946ccfd197
Osman Hernandez <osman.sist@gmail.com>
2024-11-07 12:07:00 +0100

    remove tests

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js'],
-  testMatch: ['**/*.test.js'],
-  testEnvironment: 'node'
-};
