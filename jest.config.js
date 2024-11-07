2f500519c62cd7dfb2e1eadffd415c40bf17cced
Osman Hernandez <osman.sist@gmail.com>
2024-11-07 10:37:14 +0100

    updarting dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-}
+    '^.+.tsx?$': ['ts-jest', {}]
+  }
+};
